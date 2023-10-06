9bec42fe420971ebd3ed2063537cbc2cc8568744
CodeLegg <137182565+CodeLegg@users.noreply.github.com>
2023-10-06 19:37:10 +0100

    remove js test

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,19 +93,4 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
   
-// JavaScript to dynamically adjust the mobile navigation menu's height
-  document.addEventListener("DOMContentLoaded", function () {
-  function adjustMobileMenuHeight() {
-    const mobileMenu = document.querySelector(".mobile-navigation-menu");
-    const windowHeight = window.innerHeight;
-  
-    // Set the menu's height to a percentage of the window height (e.g., 90%)
-    mobileMenu.style.height = `${windowHeight * 1}px`;
-  }
-  
-  // Call the function when the page loads and when it's resized
-  window.addEventListener("load", adjustMobileMenuHeight);
-  window.addEventListener("resize", adjustMobileMenuHeight);
-  
-});
 
