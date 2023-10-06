7f8dba0eb1fa0d19e3bc34b14b475fef35c50295
CodeLegg <137182565+CodeLegg@users.noreply.github.com>
2023-10-06 18:42:46 +0100

    update js file for browser height - test

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,3 +92,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+  
+// JavaScript to dynamically adjust the mobile navigation menu's height
+  document.addEventListener("DOMContentLoaded", function () {
+  function adjustMobileMenuHeight() {
+    const mobileMenu = document.querySelector(".mobile-navigation-menu");
+    const windowHeight = window.innerHeight;
+  
+    // Set the menu's height to a percentage of the window height (e.g., 90%)
+    mobileMenu.style.height = `${windowHeight * 0.9}px`;
+  }
+  
+  // Call the function when the page loads and when it's resized
+  window.addEventListener("load", adjustMobileMenuHeight);
+  window.addEventListener("resize", adjustMobileMenuHeight);
+  
+});
+
